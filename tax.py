55a20db2b62172256a620dd2a1967b632964d2da
cathy767 <anqi.zhu@macaulay.cuny.edu>
2017-07-26 12:05:51 -0400

    Update tax.py

--- tax.py
+++ tax.py
@@ -28,6 +28,24 @@ def web_parse():
 
 #Organizes the data into list for tax bracket
 def tax_bracket():
+    #Method 1
+    data = {    
+    '10%' : [],
+    '15%' : [],
+    '28%' : [],
+    '33%' : [],
+    '35%' : [],
+    '39.6%' : []
+    }
+    for row in table:
+    cols = row.find_all('td')
+    data['10%'].append( cols[0].get_text() )
+    data['15%'].append( cols[1].get_text() )
+    data['28%'].append( cols[2].get_text() )
+    data['33%'].append( cols[3].get_text() )
+    data['35%'].append( cols[4].get_text() )
+    data['39.6%'].append( cols[5].get_text() )
+#Method 2
     for row in table.findAll("tr")
     cells = row.findAll("td")
     if len(cells) == 7:
@@ -38,6 +56,7 @@ def tax_bracket():
         33% = cells[5].find(text=True)
         35% = cells[6].find(text=True)
         39.6% = cells[7].find(text=True)
+    
 
 if __name__ == '__main__':
     #Prints the last element of the list
